16584c9693de0aaba315d1495a22a052ca5634ff
Bhavy <bhavyajain817@gmail.com>
2025-09-22 15:27:43 +0530

    changes

--- src/components/LenisProvider.tsx
+++ src/components/LenisProvider.tsx
@@ -9,7 +9,6 @@ export default function LenisProvider({ children }: { children: React.ReactNode 
     const lenis = new Lenis({
       duration: 1.2,
       easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)), // https://www.desmos.com/calculator/brs54l4xou
-      direction: 'vertical', // vertical, horizontal
       gestureDirection: 'vertical', // vertical, horizontal, both
       smooth: true,
       mouseMultiplier: 1,
