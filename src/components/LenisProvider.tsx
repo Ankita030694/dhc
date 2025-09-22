9354e6cc755987cb219e40486753d3dfa699846d
Bhavy <bhavyajain817@gmail.com>
2025-09-22 15:31:47 +0530

    changes

--- src/components/LenisProvider.tsx
+++ src/components/LenisProvider.tsx
@@ -9,11 +9,6 @@ export default function LenisProvider({ children }: { children: React.ReactNode 
     const lenis = new Lenis({
       duration: 1.2,
       easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)), // https://www.desmos.com/calculator/brs54l4xou
-      smooth: true,
-      mouseMultiplier: 1,
-      smoothTouch: false,
-      touchMultiplier: 2,
-      infinite: false,
     });
 
     // Get scroll value
